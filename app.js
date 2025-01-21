47ae3b251c9ade572d912c5028709bf132094325
ignaciomajo <ignacio.majoo@gmail.com>
2025-01-21 10:56:18 -0300

    UPDATE app.js imprimir-lista

--- app.js
+++ app.js
@@ -8,17 +8,16 @@
 let listaAmigos = [];
 
 
-
-
-
 // *****     FUNCIONES     *****
 
 
 // Limpiar caja
 
 function limpiarCaja() {
+    // Obtener el objeto "input"
     let valorCaja = document.getElementById('amigo');
-    valorCaja.value = ''
+    // Limpar el valor para dejarlo vacío
+    valorCaja.value = '';
 }
 
 // Agregar nombres a la lista
@@ -27,19 +26,39 @@ function agregarAmigo() {
 
     let nombreIngresado = document.getElementById('amigo').value
     if (nombreIngresado === '') {
+        // Indicar al usuario que debe ingresar un valor válido
         alert('Debe ingresar un nombre, por favor, inténtelo nuevamente');
     } else {
+        // Agregar nombre ingresado por el usuario a la lista de amigos
         listaAmigos.push(nombreIngresado);
-        imprimirLista();
+        // Limpiar el contenido de la caja de texto
+        limpiarCaja();
+        // Obtener el objeto "lista" del HTML (ul)
+        const listaImprimir = document.getElementById('listaAmigos');
+        // Crear "Nodes" (enumeración) en la lista
+        const li = document.createElement('li');
+        // Cambiar el contenido del texto del "Node" creado con el nombre ingresado 
+        li.textContent = nombreIngresado;
+        // Agregar el "Node" a la lista ul
+        listaImprimir.append(li);
     }
     console.log(listaAmigos)
     
 }
 
-// Validar entrada
-
-// Visualizar lista
-
-
 
 // Sorteo aleatorio
+
+function sortearAmigo() {
+    // Almacenar la longitud de la lista para establecer el máximo índice
+    let lenLista = listaAmigos.length;
+    console.log(lenLista);
+    // Generar un número pseudo aleatorio a partir de la longitud de la lista
+    // Se utiliza floor ya que el índice mínimo es 0
+    let indiceAleatorio = Math.floor(Math.random() * lenLista);
+    console.log(indiceAleatorio);
+    console.log(listaAmigos[indiceAleatorio]);
+
+    return listaAmigos[indiceAleatorio]
+    
+}
