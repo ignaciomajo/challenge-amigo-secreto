1750a44ad0a7dade477f5009dc2d60113265d7b2
ignaciomajo <ignacio.majoo@gmail.com>
2025-01-21 09:53:07 -0300

    UPDATE app.js add-friend

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//                                              CHALLENGE AMIGO SECRETO
+
+// *****     VARIABLES     *****
+
+// Lista para almacenar los nombres ingresados por el usuario
+let listaAmigos = [];
+
+
+
+
+
+// *****     FUNCIONES     *****
+
+// Agregar nombres a la lista
+
+function agregarNombre() {
+
+    let nombreIngresado = document.getElementById('amigo').value
+    if (nombreIngresado === '') {
+        alert('Debe ingresar un nombre, por favor, inténtelo nuevamente');
+    } else {
+        listaAmigos.push(nombreIngresado);
+    }
+}
+
+// Validar entrada
+
+// Visualizar lista
+
+// Sorteo aleatorio
